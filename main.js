fb06160bde92f24abf403625f20d41e443989d14
Yoon-choi-imakesn <imakesn@gmail.com>
2022-01-17 16:49:05 +0900

    JS Navbar menu toggle

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ document.addEventListener('scroll', () => {
     }  
 });
 
-
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -21,8 +20,16 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == undefined) {
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
 
 // Handle scrolling when tapping on the "Contact Me" btn on Home
 const homeContactBtn = document.querySelector('.home__contact');
